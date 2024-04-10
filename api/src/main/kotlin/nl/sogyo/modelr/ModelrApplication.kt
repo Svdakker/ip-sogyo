00e91e709a34fa0e965728164f024dfa7d682ea9
simonvandenakker <svdakker@sogyo.nl>
2024-04-10 14:55:09 +0200

    initialized project structure

---
+++ api/src/main/kotlin/nl/sogyo/modelr/ModelrApplication.kt
@@ -0,0 +1,11 @@
+package nl.sogyo.modelr
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ModelrApplication
+
+fun main(args: Array<String>) {
+    runApplication<ModelrApplication>(*args)
+}
---
+++ api/src/test/kotlin/nl/sogyo/modelr/ModelrApplicationTests.kt
@@ -0,0 +1,13 @@
+package nl.sogyo.modelr
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ModelrApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
