d026c462be24e61bad18a74e394bb20641ca75d0
simonvandenakker <svdakker@sogyo.nl>
2024-04-17 15:10:14 +0200

    data classes are excluded from jacocoTestReport

--- domain/src/main/kotlin/nl/sogyo/modelr/ISimulationFactory.kt
+++ domain/src/main/kotlin/nl/sogyo/modelr/ISimulationFactory.kt
@@ -2,9 +2,5 @@ package nl.sogyo.modelr
 
 interface ISimulationFactory {
 
-    enum class UnitOperations{
-        BATCH_FERMENTATION
-    }
-
     fun createNewSimulation(operations: String, settings: String): ISimulation
 }
