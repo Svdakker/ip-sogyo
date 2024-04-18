ae22f5c849d28a6091c0093fa3fc12fd65b140a1
simonvandenakker <svdakker@sogyo.nl>
2024-04-18 12:11:15 +0200

    fixed tailwind integration

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import {Route, Routes} from "react-router-dom";
 import {Simulation} from "./Simulation.tsx";
+import {Home} from "./Home.tsx";
 
 export const App = () : React.JSX.Element =>  {
     return (
         <>
             <Routes>
-                <Route path={"/"} element={<Simulation/>}/>
+                <Route path={"/"} element={<Home/>}/>
+                <Route path={"/simulation"} element={<Simulation/>}/>
             </Routes>
         </>
     )
