cff3d4f42569731c5175ba50d829a014a36d2f59
simonvandenakker <svdakker@sogyo.nl>
2024-04-19 16:30:46 +0200

    Added reactor settings in fetch call input

--- domain/src/main/kotlin/nl/sogyo/modelr/data/BioreactorProperties.kt
+++ domain/src/main/kotlin/nl/sogyo/modelr/data/BioreactorProperties.kt
@@ -5,7 +5,7 @@ data class BioreactorProperties(
     val workingVolume: Double = 52.5, //m3
     val vesselHeight: Double = 9.29, //m
     val vesselWidth: Double = 3.10, //m (diameter)
-    val impellerType: String = "Rushton turbine",
+    val impellerType: String = "rushton turbine",
     val impellerDiameter: Double = 0.97, //m
     val impellerFlowNumber: Double = 0.72,
     val impellerPowerNumber: Double = 5.2,
