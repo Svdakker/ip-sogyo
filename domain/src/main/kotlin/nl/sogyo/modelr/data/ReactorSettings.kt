9c4eb38b451833e6b95023a73275f6d9ee66de01
simonvandenakker <svdakker@sogyo.nl>
2024-04-22 13:49:33 +0200

    removed hard-coded properties from reactor settings

--- domain/src/main/kotlin/nl/sogyo/modelr/data/ReactorSettings.kt
+++ domain/src/main/kotlin/nl/sogyo/modelr/data/ReactorSettings.kt
@@ -1,13 +1,13 @@
 package nl.sogyo.modelr.data
 
 data class ReactorSettings(
-        val nominalVolume: Double = 70.0, //m3
-        val workingVolume: Double = 52.5, //m3
-        val height: Double = 9.29, //m
-        val width: Double = 3.10, //m (diameter)
-        val impellerType: String = "rushton turbine",
-        val numberOfImpellers: Int = 4,
-        val agitatorSpeed: Double = 2.5, //s-1
+        val nominalVolume: Double, //m3
+        val workingVolume: Double, //m3
+        val height: Double, //m
+        val width: Double, //m (diameter)
+        val impellerType: String,
+        val numberOfImpellers: Int,
+        val agitatorSpeed: Double, //s-1
         val impellerDiameter: Double = 0.97, //m
         val impellerFlowNumber: Double = 0.72,
         val impellerPowerNumber: Double = 5.2,
