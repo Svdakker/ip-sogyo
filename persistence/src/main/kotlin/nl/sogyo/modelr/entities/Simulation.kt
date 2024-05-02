42d0f8232e475c2f82938fb6368e025d2d0ea15d
simonvandenakker <svdakker@sogyo.nl>
2024-05-02 14:03:13 +0200

    changed simulation id column name in db

--- persistence/src/main/kotlin/nl/sogyo/modelr/entities/Simulation.kt
+++ persistence/src/main/kotlin/nl/sogyo/modelr/entities/Simulation.kt
@@ -5,7 +5,7 @@ import jakarta.persistence.*
 @Entity
 class Simulation(
     @OneToMany(cascade = [CascadeType.ALL])
-    @JoinColumn(name = "batch_id", nullable = true)
+    @JoinColumn(name = "simulation_id", nullable = true)
     var batchCultivation: List<BatchCultivation?> = emptyList(), //foreign key to possible BatchCultivations
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
